b96251dd452c87582385b633b07c942864e2657e
Danny <danny@futureplay.co>
2022-10-28 15:55:40 +0900

    feat : Add recoil root in app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css';
 
-import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import type { AppProps } from 'next/app';
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
