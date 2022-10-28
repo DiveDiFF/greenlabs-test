1faf98ef68ec40bdd908fec6ce4b06af76556bd0
Danny <danny@futureplay.co>
2022-10-28 18:09:34 +0900

    feat : Add default page and set mock data to atom

---
+++ src/pages/product-list.tsx
@@ -0,0 +1,12 @@
+import { useRecoilValue } from 'recoil';
+import { productList } from 'recoil/products';
+
+export default function ProductListPage() {
+  const data = useRecoilValue(productList);
+  console.log({ data });
+  return (
+    <>
+      <p>프로덕트 리스트</p>
+    </>
+  );
+}
---
+++ src/recoil/products.ts
@@ -0,0 +1,7 @@
+import mockData from 'db/data.json';
+import { atom } from 'recoil';
+
+export const productList = atom({
+  key: "productList",
+  default: mockData,
+});
