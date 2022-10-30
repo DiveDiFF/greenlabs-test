9c09adb00946b5353f23d88112fcc75f907ae1ed
Danny <danny@futureplay.co>
2022-10-30 21:11:13 +0900

    feat : Add calculate price in receipt section and submit button

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,21 +1,39 @@
 import Divider from 'components/common/Divider';
+import { useRouter } from 'next/router';
+import { MouseEvent, useCallback, useMemo } from 'react';
 import { useRecoilValue } from 'recoil';
 import { selectedCartList } from 'recoil/cart';
 
 import ApplyCoupon from './ApplyCoupon';
 import CartItemList from './CartItemList';
+import styles from './MyCart.module.css';
 import Receipt from './Receipt';
 
 export default function MyCart() {
   const selectedCartItem = useRecoilValue(selectedCartList);
+  const selectedCartLength = useMemo(() => selectedCartItem?.length, [selectedCartItem.length]);
+  const { push } = useRouter();
+
+  const { button } = styles;
+
+  const handleClickPayButton = useCallback(
+    (_event: MouseEvent<HTMLButtonElement>) => {
+      console.log('234234');
+      push('/payment');
+    },
+    [push],
+  );
+
+  console.log(selectedCartLength);
   return (
     <>
       <CartItemList />
-      <Divider />
       <ApplyCoupon />
       <Divider />
       <Receipt />
-      <button>상품 {selectedCartItem.length}개 결제하기</button>
+      <button className={button} onClick={handleClickPayButton} disabled={!selectedCartLength}>
+        상품 {selectedCartLength}개 결제하기
+      </button>
     </>
   );
 }
--- src/recoil/cart.ts
+++ src/recoil/cart.ts
@@ -1,6 +1,8 @@
 import { atom, selector } from 'recoil';
 import { CartItem } from 'typings/cart';
 
+import { appliedCouponList } from './coupon';
+
 export const myCartList = atom<CartItem[]>({
   key: 'myCartList',
   default: [],
@@ -11,5 +13,41 @@ export const selectedCartList = selector<CartItem[]>({
   get: ({ get }) => {
     const carts = get(myCartList);
     return carts.filter((item) => item.isSelected);
+  },
+});
+
+export const calculateTotalPrice = selector<number>({
+  key: 'calculateTotalPrice',
+  get: ({ get }) => {
+    const selectedCartItems = get(selectedCartList);
+
+    return selectedCartItems.reduce((acc, currentVal) => currentVal?.productData?.price * currentVal?.quantity + acc, 0);
+  },
+});
+
+export const calculateTotalDiscount = selector<number>({
+  key: 'calculateTotalDiscount',
+  get: ({ get }) => {
+    const appliedCoupons = get(appliedCouponList);
+    const totalPrice = get(calculateTotalPrice);
+
+    return appliedCoupons.reduce((acc, { discountPercent, discountPrice }) => {
+      if (discountPercent) {
+        acc += Math.floor(totalPrice * (discountPercent / 100));
+      }
+      if (discountPrice) {
+        acc += discountPrice;
+      }
+      return acc;
+    }, 0);
+  },
+});
+
+export const calculateShippingFee = selector({
+  key: 'calculateShippingFee',
+  get: ({ get }) => {
+    const appliedCoupons = get(appliedCouponList);
+
+    return appliedCoupons.some((coupon) => coupon.freeShipping) ? 0 : 3000;
   },
 });
