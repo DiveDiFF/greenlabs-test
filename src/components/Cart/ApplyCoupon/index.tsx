6f850728051a3485f08a00edee9e7d61c0bbdb3c
Danny <danny@futureplay.co>
2022-10-30 19:55:34 +0900

    feat : Add condition of applied chip in cart page

--- src/components/Cart/ApplyCoupon/index.tsx
+++ src/components/Cart/ApplyCoupon/index.tsx
@@ -2,14 +2,15 @@ import Chip from 'components/common/Chip';
 import { useRouter } from 'next/router';
 import { MouseEvent, useCallback } from 'react';
 import { useRecoilValue } from 'recoil';
-import { myCouponList } from 'recoil/coupon';
+import { appliedCouponList, myCouponList } from 'recoil/coupon';
 
 import styles from './ApplyCoupon.module.css';
 
 export default function ApplyCoupon() {
+  const { container, applyButton, numberText } = styles;
   const { push } = useRouter();
   const couponData = useRecoilValue(myCouponList);
-  const { container, applyButton, numberText } = styles;
+  const appliedCouponData = useRecoilValue(appliedCouponList);
 
   const handleClickCouponSection = useCallback(
     (_event: MouseEvent<HTMLButtonElement>) => {
@@ -22,8 +23,7 @@ export default function ApplyCoupon() {
     <section className={container}>
       <button onClick={handleClickCouponSection} className={applyButton}>
         <h5>할인쿠폰</h5>
-        <span>-1000원</span>
-        <Chip label="적용완료" variant="confirm" />
+        {!!appliedCouponData?.length && <Chip label="적용완료" variant="confirm" />}
         <p className={numberText}>{couponData?.length}개 보유</p>
       </button>
     </section>
